290e73b14df7358b629f4aa05b06f01fa87cab4d
kakoga <kkoga@alumni.nd.edu>
2016-03-13 13:46:11 -0700

    access to all records complete

--- spec/features/animal_interfaces_spec.rb
+++ spec/features/animal_interfaces_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-#story 1
+# story 1
 RSpec.feature "AnimalInterfaces", type: :feature do
-  describe "As a ranger on the home page" do
+  describe "As a ranger on the homepage" do
     it "I can go to the homepage" do
       visit "/"
       expect(page).to have_content("Welcome to Wildlife Tracker")
     end
 
-    #story 2
+    # story 2
     it "I can create an animal and save it in the database from the home page" do
       visit "/"
       fill_in 'Name', with: 'Monkey'
@@ -18,5 +18,11 @@ RSpec.feature "AnimalInterfaces", type: :feature do
       #test fails because there is no model or table.
     end
 
+    # story 3
+    it "I can view all records" do
+      visit "/"
+      expect(page).to have_content("Click here to view all records")
+      click_button 'All Records'
+    end
   end
 end
